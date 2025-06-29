41965770cda95452789382f55d6a3d22cf195c25
khosta77 <puwerfulpants@gmail.com>
2025-06-29 16:15:28 +0300

    Применил ко всем С++ файлам clang-format

--- system/include/cmsis/stm32g0xx.h
+++ system/include/cmsis/stm32g0xx.h
@@ -38,7 +38,8 @@
 #define STM32G0xx_H
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif /* __cplusplus */
 
 /** @addtogroup Library_configuration_section
@@ -48,18 +49,20 @@ extern "C" {
 /**
  * @brief STM32 Family
  */
-#if !defined(STM32G0)
+#if !defined( STM32G0 )
 #define STM32G0
 #endif /* STM32G0 */
 
-/* Uncomment the line below according to the target STM32G0 device used in your
-   application
-  */
+    /* Uncomment the line below according to the target STM32G0 device used in
+       your application
+      */
 
-#if !defined(STM32G071xx) && !defined(STM32G081xx) && !defined(STM32G070xx) && \
-    !defined(STM32G030xx) && !defined(STM32G031xx) && !defined(STM32G041xx) && \
-    !defined(STM32G0B0xx) && !defined(STM32G0B1xx) && !defined(STM32G0C1xx) && \
-    !defined(STM32G050xx) && !defined(STM32G051xx) && !defined(STM32G061xx)
+#if !defined( STM32G071xx ) && !defined( STM32G081xx ) &&                      \
+    !defined( STM32G070xx ) && !defined( STM32G030xx ) &&                      \
+    !defined( STM32G031xx ) && !defined( STM32G041xx ) &&                      \
+    !defined( STM32G0B0xx ) && !defined( STM32G0B1xx ) &&                      \
+    !defined( STM32G0C1xx ) && !defined( STM32G050xx ) &&                      \
+    !defined( STM32G051xx ) && !defined( STM32G061xx )
 /* #define STM32G0B0xx */ /*!< STM32G0B0xx Devices */
 /* #define STM32G0B1xx */ /*!< STM32G0B1xx Devices */
 /* #define STM32G0C1xx */ /*!< STM32G0C1xx Devices */
@@ -78,7 +81,7 @@ extern "C" {
         devices, you can define the device in your toolchain compiler
    preprocessor.
   */
-#if !defined(USE_HAL_DRIVER)
+#if !defined( USE_HAL_DRIVER )
 /**
  * @brief Comment the line below if you will not use the peripherals drivers.
    In this case, these drivers will not be included and the application code
@@ -90,65 +93,79 @@ extern "C" {
 /**
  * @brief CMSIS Device version number $VERSION$
  */
-#define __STM32G0_CMSIS_VERSION_MAIN (0x01U) /*!< [31:24] main version */
-#define __STM32G0_CMSIS_VERSION_SUB1 (0x04U) /*!< [23:16] sub1 version */
-#define __STM32G0_CMSIS_VERSION_SUB2 (0x01U) /*!< [15:8]  sub2 version */
-#define __STM32G0_CMSIS_VERSION_RC (0x00U)   /*!< [7:0]  release candidate */
+#define __STM32G0_CMSIS_VERSION_MAIN ( 0x01U ) /*!< [31:24] main version */
+#define __STM32G0_CMSIS_VERSION_SUB1 ( 0x04U ) /*!< [23:16] sub1 version */
+#define __STM32G0_CMSIS_VERSION_SUB2 ( 0x01U ) /*!< [15:8]  sub2 version */
+#define __STM32G0_CMSIS_VERSION_RC ( 0x00U )   /*!< [7:0]  release candidate */
 #define __STM32G0_CMSIS_VERSION                                                \
-  ((__STM32G0_CMSIS_VERSION_MAIN << 24) |                                      \
-   (__STM32G0_CMSIS_VERSION_SUB1 << 16) |                                      \
-   (__STM32G0_CMSIS_VERSION_SUB2 << 8) | (__STM32G0_CMSIS_VERSION_RC))
+    ( ( __STM32G0_CMSIS_VERSION_MAIN << 24 ) |                                 \
+      ( __STM32G0_CMSIS_VERSION_SUB1 << 16 ) |                                 \
+      ( __STM32G0_CMSIS_VERSION_SUB2 << 8 ) | ( __STM32G0_CMSIS_VERSION_RC ) )
 
-/**
- * @}
- */
+    /**
+     * @}
+     */
 
-/** @addtogroup Device_Included
- * @{
- */
+    /** @addtogroup Device_Included
+     * @{
+     */
 
-#if defined(STM32G0B1xx)
+#if defined( STM32G0B1xx )
 #include "stm32g0b1xx.h"
-#elif defined(STM32G0C1xx)
+#elif defined( STM32G0C1xx )
 #include "stm32g0c1xx.h"
-#elif defined(STM32G0B0xx)
+#elif defined( STM32G0B0xx )
 #include "stm32g0b0xx.h"
-#elif defined(STM32G071xx)
+#elif defined( STM32G071xx )
 #include "stm32g071xx.h"
-#elif defined(STM32G081xx)
+#elif defined( STM32G081xx )
 #include "stm32g081xx.h"
-#elif defined(STM32G070xx)
+#elif defined( STM32G070xx )
 #include "stm32g070xx.h"
-#elif defined(STM32G031xx)
+#elif defined( STM32G031xx )
 #include "stm32g031xx.h"
-#elif defined(STM32G041xx)
+#elif defined( STM32G041xx )
 #include "stm32g041xx.h"
-#elif defined(STM32G030xx)
+#elif defined( STM32G030xx )
 #include "stm32g030xx.h"
-#elif defined(STM32G051xx)
+#elif defined( STM32G051xx )
 #include "stm32g051xx.h"
-#elif defined(STM32G061xx)
+#elif defined( STM32G061xx )
 #include "stm32g061xx.h"
-#elif defined(STM32G050xx)
+#elif defined( STM32G050xx )
 #include "stm32g050xx.h"
 #else
 #error                                                                         \
     "Please select first the target STM32G0xx device used in your application (in stm32g0xx.h file)"
 #endif
 
-/**
- * @}
- */
+    /**
+     * @}
+     */
 
-/** @addtogroup Exported_types
- * @{
- */
-typedef enum { RESET = 0, SET = !RESET } FlagStatus, ITStatus;
+    /** @addtogroup Exported_types
+     * @{
+     */
+    typedef enum
+    {
+        RESET = 0,
+        SET = !RESET
+    } FlagStatus,
+        ITStatus;
 
-typedef enum { DISABLE = 0, ENABLE = !DISABLE } FunctionalState;
-#define IS_FUNCTIONAL_STATE(STATE) (((STATE) == DISABLE) || ((STATE) == ENABLE))
+    typedef enum
+    {
+        DISABLE = 0,
+        ENABLE = !DISABLE
+    } FunctionalState;
+#define IS_FUNCTIONAL_STATE( STATE )                                           \
+    ( ( ( STATE ) == DISABLE ) || ( ( STATE ) == ENABLE ) )
 
-typedef enum { SUCCESS = 0, ERROR = !SUCCESS } ErrorStatus;
+    typedef enum
+    {
+        SUCCESS = 0,
+        ERROR = !SUCCESS
+    } ErrorStatus;
 
 /**
  * @}
@@ -157,66 +174,70 @@ typedef enum { SUCCESS = 0, ERROR = !SUCCESS } ErrorStatus;
 /** @addtogroup Exported_macros
  * @{
  */
-#define SET_BIT(REG, BIT) ((REG) |= (BIT))
+#define SET_BIT( REG, BIT ) ( ( REG ) |= ( BIT ) )
 
-#define CLEAR_BIT(REG, BIT) ((REG) &= ~(BIT))
+#define CLEAR_BIT( REG, BIT ) ( ( REG ) &= ~( BIT ) )
 
-#define READ_BIT(REG, BIT) ((REG) & (BIT))
+#define READ_BIT( REG, BIT ) ( ( REG ) & ( BIT ) )
 
-#define CLEAR_REG(REG) ((REG) = (0x0))
+#define CLEAR_REG( REG ) ( ( REG ) = ( 0x0 ) )
 
-#define WRITE_REG(REG, VAL) ((REG) = (VAL))
+#define WRITE_REG( REG, VAL ) ( ( REG ) = ( VAL ) )
 
-#define READ_REG(REG) ((REG))
+#define READ_REG( REG ) ( ( REG ) )
 
-#define MODIFY_REG(REG, CLEARMASK, SETMASK)                                    \
-  WRITE_REG((REG), (((READ_REG(REG)) & (~(CLEARMASK))) | (SETMASK)))
+#define MODIFY_REG( REG, CLEARMASK, SETMASK )                                  \
+    WRITE_REG( ( REG ), ( ( ( READ_REG( REG ) ) & ( ~( CLEARMASK ) ) ) |       \
+                          ( SETMASK ) ) )
 
 /* Use of interrupt control for register exclusive access */
 /* Atomic 32-bit register access macro to set one or several bits */
-#define ATOMIC_SET_BIT(REG, BIT)                                               \
-  do {                                                                         \
-    uint32_t primask;                                                          \
-    primask = __get_PRIMASK();                                                 \
-    __set_PRIMASK(1);                                                          \
-    SET_BIT((REG), (BIT));                                                     \
-    __set_PRIMASK(primask);                                                    \
-  } while (0)
+#define ATOMIC_SET_BIT( REG, BIT )                                             \
+    do                                                                         \
+    {                                                                          \
+        uint32_t primask;                                                      \
+        primask = __get_PRIMASK();                                             \
+        __set_PRIMASK( 1 );                                                    \
+        SET_BIT( ( REG ), ( BIT ) );                                           \
+        __set_PRIMASK( primask );                                              \
+    } while ( 0 )
 
 /* Atomic 32-bit register access macro to clear one or several bits */
-#define ATOMIC_CLEAR_BIT(REG, BIT)                                             \
-  do {                                                                         \
-    uint32_t primask;                                                          \
-    primask = __get_PRIMASK();                                                 \
-    __set_PRIMASK(1);                                                          \
-    CLEAR_BIT((REG), (BIT));                                                   \
-    __set_PRIMASK(primask);                                                    \
-  } while (0)
+#define ATOMIC_CLEAR_BIT( REG, BIT )                                           \
+    do                                                                         \
+    {                                                                          \
+        uint32_t primask;                                                      \
+        primask = __get_PRIMASK();                                             \
+        __set_PRIMASK( 1 );                                                    \
+        CLEAR_BIT( ( REG ), ( BIT ) );                                         \
+        __set_PRIMASK( primask );                                              \
+    } while ( 0 )
 
 /* Atomic 32-bit register access macro to clear and set one or several bits */
-#define ATOMIC_MODIFY_REG(REG, CLEARMSK, SETMASK)                              \
-  do {                                                                         \
-    uint32_t primask;                                                          \
-    primask = __get_PRIMASK();                                                 \
-    __set_PRIMASK(1);                                                          \
-    MODIFY_REG((REG), (CLEARMSK), (SETMASK));                                  \
-    __set_PRIMASK(primask);                                                    \
-  } while (0)
+#define ATOMIC_MODIFY_REG( REG, CLEARMSK, SETMASK )                            \
+    do                                                                         \
+    {                                                                          \
+        uint32_t primask;                                                      \
+        primask = __get_PRIMASK();                                             \
+        __set_PRIMASK( 1 );                                                    \
+        MODIFY_REG( ( REG ), ( CLEARMSK ), ( SETMASK ) );                      \
+        __set_PRIMASK( primask );                                              \
+    } while ( 0 )
 
 /* Atomic 16-bit register access macro to set one or several bits */
-#define ATOMIC_SETH_BIT(REG, BIT) ATOMIC_SET_BIT(REG, BIT)
+#define ATOMIC_SETH_BIT( REG, BIT ) ATOMIC_SET_BIT( REG, BIT )
 
 /* Atomic 16-bit register access macro to clear one or several bits */
-#define ATOMIC_CLEARH_BIT(REG, BIT) ATOMIC_CLEAR_BIT(REG, BIT)
+#define ATOMIC_CLEARH_BIT( REG, BIT ) ATOMIC_CLEAR_BIT( REG, BIT )
 
 /* Atomic 16-bit register access macro to clear and set one or several bits */
-#define ATOMIC_MODIFYH_REG(REG, CLEARMSK, SETMASK)                             \
-  ATOMIC_MODIFY_REG(REG, CLEARMSK, SETMASK)
+#define ATOMIC_MODIFYH_REG( REG, CLEARMSK, SETMASK )                           \
+    ATOMIC_MODIFY_REG( REG, CLEARMSK, SETMASK )
 
-/*#define POSITION_VAL(VAL)     (__CLZ(__RBIT(VAL)))*/
-/**
- * @}
- */
+    /*#define POSITION_VAL(VAL)     (__CLZ(__RBIT(VAL)))*/
+    /**
+     * @}
+     */
 
 #ifdef __cplusplus
 }
