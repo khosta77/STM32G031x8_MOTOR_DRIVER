5e6c160ce3b478908e9c2c276c2cc3ba4350143b
khosta77 <puwerfulpants@gmail.com>
2025-07-19 16:49:17 +0300

    Внес правки в код, USART1 работает, начал писать прерывания

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include <cmath>
 #include <stdint.h>
 
+const uint8_t USART1_NVIC_PRIORITY = 4;
+const uint8_t PB3_NVIC_PRIORITY = 2;
+const uint8_t PA4_PA5_NVIC_PRIORITY = 3;
+
 void usart_write_byte( const uint8_t data )
 {
     /*
@@ -60,7 +64,7 @@ void usart_init( uint32_t baud )
     USART1->CR1 |= USART_CR1_UE;
 
     NVIC_EnableIRQ( USART1_IRQn );
-    NVIC_SetPriority( USART1_IRQn, 2 );
+    NVIC_SetPriority( USART1_IRQn, USART1_NVIC_PRIORITY );
 }
 
 uint8_t MSG_STATUS = 0x00;
@@ -91,8 +95,8 @@ void __attribute__( ( interrupt, used ) ) USART1_IRQHandler( void )
         uint32_t primask;
         uint16_t end;
 
-        primask = __get_PRIMASK(); /* get the interrupt status */
-        __disable_irq();           /* disable IRQs, this will modify PRIMASK */
+        // primask = __get_PRIMASK(); // get the interrupt status
+        //__disable_irq();           // disable IRQs, this will modify PRIMASK
 
         if ( READ_BIT( msfm_mrk, MSFM_MRK_MODE ) == MSFM_MRK_MODE )
         {
@@ -117,9 +121,39 @@ void __attribute__( ( interrupt, used ) ) USART1_IRQHandler( void )
         }
 
         SET_BIT( msfm_mrk, MSG_STATUS );
-        __set_PRIMASK( primask ); /* restore interrupt state */
+        //__set_PRIMASK( primask ); // restore interrupt state
 
         return;
+    }
+}
+
+void __attribute__( ( interrupt, used ) ) EXTI4_15_IRQHandler( void )
+{
+    // Проверяем флаги прерываний для PA4 и PA5
+    if ( EXTI->RPR1 & EXTI_RPR1_RPIF4 )
+    { // PA4
+        // Ваш код обработки прерывания для PA4
+        EXTI->RPR1 = EXTI_RPR1_RPIF4; // Сброс флага
+        GPIOA->ODR |= GPIO_ODR_OD0;
+        GPIOA->ODR &= ~GPIO_ODR_OD0;
+    }
+
+    if ( EXTI->RPR1 & EXTI_RPR1_RPIF5 )
+    { // PA5
+        // Ваш код обработки прерывания для PA5
+        EXTI->RPR1 = EXTI_RPR1_RPIF5; // Сброс флага
+        GPIOA->ODR |= GPIO_ODR_OD1;
+        GPIOA->ODR &= ~GPIO_ODR_OD1;
+    }
+}
+
+void EXTI2_3_IRQHandler( void )
+{
+    // Проверяем флаг прерывания для PB3
+    if ( EXTI->RPR1 & EXTI_RPR1_RPIF3 )
+    { // PB3
+        // Ваш код обработки прерывания для PB3
+        EXTI->RPR1 = EXTI_RPR1_RPIF3; // Сброс флага
     }
 }
 } // extern "C"
@@ -140,6 +174,39 @@ int main( void )
     GPIOA->OTYPER &= ~GPIO_OTYPER_OT1;
     GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD1;
 
+    ////
+    RCC->IOPENR |= RCC_IOPENR_GPIOBEN;
+
+    // Настраиваем PB3, PA4, PA5 на вход (пример)
+    GPIOB->MODER &= ~GPIO_MODER_MODE3; // PB3 input
+    GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD3;
+    GPIOB->PUPDR |= GPIO_PUPDR_PUPD3_1; // pull-down
+
+    GPIOA->MODER &= ~( GPIO_MODER_MODE4 | GPIO_MODER_MODE5 ); // PA4, PA5 input
+    GPIOA->PUPDR &= ~( GPIO_PUPDR_PUPD4 | GPIO_PUPDR_PUPD5 );
+    GPIOA->PUPDR |= GPIO_PUPDR_PUPD4_1 | GPIO_PUPDR_PUPD5_1;
+
+    // Настраиваем EXTI для PB3 (EXTI3)
+    EXTI->EXTICR[0] &= ~EXTI_EXTICR1_EXTI3;
+    EXTI->EXTICR[0] |= ( 1 << EXTI_EXTICR1_EXTI3_Pos ); // PB3 для EXTI3
+
+    // Настраиваем EXTI для PA4 (EXTI4) и PA5 (EXTI5)
+    EXTI->EXTICR[1] &= ~( EXTI_EXTICR2_EXTI4 | EXTI_EXTICR2_EXTI5 );
+    // PA4 и PA5 уже подключены к EXTI4 и EXTI5 (0b0000)
+
+    // Включаем триггер по фронту (настраивайте по необходимости)
+    EXTI->RTSR1 |= EXTI_RTSR1_RT3 | EXTI_RTSR1_RT4 | EXTI_RTSR1_RT5; // Rising trigger
+    // EXTI->FTSR1 |= EXTI_FTSR1_FT3 | EXTI_FTSR1_FT4 | EXTI_FTSR1_FT5; // Falling trigger
+
+    // Разрешаем прерывания
+    EXTI->IMR1 |= EXTI_IMR1_IM3 | EXTI_IMR1_IM4 | EXTI_IMR1_IM5;
+
+    // Настраиваем приоритеты и включаем прерывания в NVIC
+    NVIC_SetPriority( EXTI2_3_IRQn, PB3_NVIC_PRIORITY ); // Приоритет для EXTI2_3 (PB3)
+    NVIC_EnableIRQ( EXTI2_3_IRQn );
+
+    NVIC_SetPriority( EXTI4_15_IRQn, PA4_PA5_NVIC_PRIORITY ); // Приоритет для EXTI4_15 (PA4, PA5)
+    NVIC_EnableIRQ( EXTI4_15_IRQn );
     /*
      * Какая то херня с прерываниями
      * */
@@ -155,7 +222,6 @@ int main( void )
         {
             if ( READ_BIT( msfm_mrk, MSG_STATUS ) == MSG_STATUS )
             {
-                GPIOA->ODR |= GPIO_ODR_OD0;
                 CLEAR_BIT( msfm_mrk, MSG_STATUS );
                 if ( READ_BIT( msfm_mrk, MSFM_MRK_MODE ) )
                 {
@@ -165,7 +231,6 @@ int main( void )
                 {
                     ptr = &msfm1._frame[0];
                 }
-                GPIOA->ODR &= ~GPIO_ODR_OD0;
                 break;
             }
         }
